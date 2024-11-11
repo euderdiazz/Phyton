0c378bf0c52cf950503a4be43ccbd18c6ac360b3
Euder Dias <143039330+euderdiazz@users.noreply.github.com>
2024-11-10 23:04:07 -0300

    primeiras linhas

---
+++ print (olá!).py
@@ -0,0 +1,13 @@
+print ("olá!")
+print(10+20*30)
+print(4**2/30)
+print(10%3*10**2+1-10*4/2)
+print("Euder")
+a=3
+b=5
+c=7
+print(2*a+3*b)
+print(a+b+c)
+salário=750
+aumento=15
+print(salário+(salário*15/100))
